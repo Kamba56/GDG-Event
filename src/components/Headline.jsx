263bb0e54e66a4f5a97476a6cb220e2fb8b8cf33
Toma John Musa <tomajohn316@yahoo.com>
2023-02-11 04:48:20 +0100

    Add grid for mobile

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,7 +1,14 @@
 import './component_css/headline.css';
 
 const Headline = () => (
-  <section>I work</section>
+  <section className='headline-grid'>
+    <div className='one item-a'></div>
+    <div className='one item-b'></div>
+    <div className='one item-c'></div>
+    <div className='one item-d'></div>
+    <div className='one item-e'></div>
+    <div className='one item-f'></div>
+  </section>
 );
 
 export default Headline;
