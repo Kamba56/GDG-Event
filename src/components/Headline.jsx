2b967e2795a00bac6aac83b0ee25328943c65b64
Toma John Musa <tomajohn316@yahoo.com>
2023-02-11 05:38:50 +0100

    Add headline section

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -2,10 +2,13 @@ import './component_css/headline.css';
 
 const Headline = () => (
   <section className='headline-grid'>
-    <div className='one item-a'></div>
+    <div className='one item-a'>
+      <p>Let’s build a 
+      <span> better world together</span></p>
+    </div>
     <div className='one item-b'></div>
     <div className='one item-c'></div>
-    <div className='one item-d'></div>
+    <div className='one item-d'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, provident laudantium! Asperiores reiciendis quis repudi</div>
     <div className='one item-e'></div>
     <div className='one item-f'></div>
   </section>
